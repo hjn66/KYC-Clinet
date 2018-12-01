7cd122d17476fd01ee0fc74c259ad97003e51719
hjn66 <h.niloofar@gmail.com>
2018-12-01 14:34:10 +0330

    Save File

--- app/src/main/java/com/areatak/kycclient/MainActivity.java
+++ app/src/main/java/com/areatak/kycclient/MainActivity.java
@@ -1,18 +1,34 @@
 package com.areatak.kycclient;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.util.Xml;
 import android.view.View;
 import android.widget.Button;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.xmlpull.v1.XmlSerializer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Timer;
@@ -23,6 +39,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private String registerStatus;
     private Timer timer;
+    private static final int RC_HANDLE_STORAGE_PERM = 3;
+    private static final String LOG_TAG = "KYC.MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +49,25 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (isExternalStorageWritable()) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                final String[] permissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_STORAGE_PERM);
 
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }else{
+                createDirectory();
+            }
+//            getPublicAlbumStorageDir("KYC");
+
+        }
         SharedPreferences sharedPref = this.getSharedPreferences("PROFILE", Context.MODE_PRIVATE);
         registerStatus = sharedPref.getString(getString(R.string.register_status), "Pending");
         applyStatus(registerStatus);
@@ -57,6 +94,56 @@ public class MainActivity extends Activity implements View.OnClickListener {
         timer.schedule(doAsynchronousTask, 0, 1000); //execute in every 1 sec
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != RC_HANDLE_STORAGE_PERM) {
+            Log.d(LOG_TAG, "Got unexpected permission result: " + requestCode);
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            return;
+        }
+
+        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.d(LOG_TAG, "Storage permission granted");
+            createDirectory();
+
+            // we have permission, so create the camerasource
+            return;
+        }
+
+        Log.e(LOG_TAG, "Storage permission not granted: results len = " + grantResults.length +
+                " Result code = " + (grantResults.length > 0 ? grantResults[0] : "(empty)"));
+
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                finish();
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("KYC Client")
+                .setMessage(R.string.no_camera_permission)
+                .setPositiveButton(R.string.ok, listener)
+                .show();
+    }
+
+    private String writeXml(){
+        XmlSerializer serializer = Xml.newSerializer();
+        StringWriter writer = new StringWriter();
+        try {
+            serializer.setOutput(writer);
+            serializer.startDocument("UTF-8", true);
+            serializer.startTag("", "messages");
+            serializer.text("Salam");
+            serializer.endTag("", "messages");
+            serializer.endDocument();
+            return writer.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private void applyStatus(String status) {
         Button loginButton = findViewById(R.id.button_login);
         if (status.equals("Approved")) {
@@ -76,6 +163,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         String urlString = protocol + "://" + serverAddress + ":" + serverPort + getString(R.string.check_status_subURL) + nonce;
         URL url = new URL(urlString);
+
         try {
             String registerStatusResult = new CheckRegisterStatus().execute(url).get();
             JSONObject jsonObject = new JSONObject(registerStatusResult);
@@ -97,6 +185,48 @@ public class MainActivity extends Activity implements View.OnClickListener {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+    private void createDirectory() {
+        Context context = getApplicationContext();
+//        File file = new File(context.getExternalFilesDir(
+//                Environment.DIRECTORY_PICTURES), "ssss");
+//        File file = new File(Environment.getExternalStoragePublicDirectory(
+//                Environment.DIRECTORY_PICTURES), "SS");
+        File file = new File(Environment.getExternalStorageDirectory()+File.separator+"11KYC");
+        if (!file.exists()) {
+            file.mkdirs();
+
+            Log.w("DEBUG", "Created default directory.");
+
+        }
+        File gpxfile = new File(file, "1.txt");
+        try {
+            FileWriter writer = new FileWriter(gpxfile);
+            writer.append(writeXml());
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public File getPublicAlbumStorageDir(String albumName) {
+        // Get the directory for the user's public pictures directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), albumName);
+        if (!file.mkdirs()) {
+            Log.e("DEBUG", "Directory not created");
+        }
+        return file;
     }
 
     /**
